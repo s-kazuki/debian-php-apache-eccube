a9e8e9a10599ee78aa7da54edd020b63cc5ff87e
SUGIYAMA Kazuki <sugiyama.kazuki.26x@gmail.com>
2019-02-27 14:34:04 +0900

    update

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+# www-dataのUIDを変更する
+if [ -n "${HOST_UID}" -a "${HOST_UID}" != "$(id -u www-data)" ]; then
+    usermod -u "${HOST_UID}" www-data
+fi
+
+# リモートデバッグの設定は環境変数だけでは対応できないため動的に追記
+xdebug=$(cat << EOS
+xdebug.remote_host=$(ip route | awk 'NR==1 {print $3}')
+xdebug.remote_port=${XDEBUG_PORT}
+EOS
+)
+echo "${xdebug}" >> /usr/local/etc/php/conf.d/my.ini
+
+if [ -e "composer.json" ]; then
+    sudo -u www-data composer install
+fi
+
+if [ -e "package.json" ]; then
+    sudo -u www-data npm install
+fi
+
+if [ "${1#-}" != "$1" ]; then
+    set -- apache2-foreground "$@"
+fi
+
+exec "$@"
